acdc73a0d9b22fea6c2a31caca666eb6139603bb
Jacques <jacques@trixta.com>
2021-09-15 18:25:03 +0200

    ci: publish release

--- release.config.js
+++ release.config.js
@@ -11,7 +11,7 @@ module.exports = {
     '@semantic-release/commit-analyzer',
     '@semantic-release/release-notes-generator',
     '@semantic-release/changelog',
-    ['@semantic-release/npm', { npmPublish: true }],
+    ['@semantic-release/npm', { npmPublish: false }],
     [
       '@semantic-release/git',
       {

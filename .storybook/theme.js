ae5a0d47926840bb6582beca0efd3d997b19a779
Jacques <jacques@trixta.com>
2021-10-20 09:02:15 +0200

    feat: useTrixtaData hook

--- .storybook/theme.js
+++ .storybook/theme.js
@@ -2,9 +2,9 @@ import { create } from '@storybook/theming/create';
 import packageJson from '../package.json';
 
 export default create({
-  base: 'light',
+  base: 'dark',
   brandTitle: `@trixtateam/trixta-js ${packageJson.version}`,
   brandUrl: 'https://trixta.com',
   brandImage: 'https://raw.githubusercontent.com/trixtateam/trixta-js/master/docs/images/trixta-logo.png',
-  
+
 });
